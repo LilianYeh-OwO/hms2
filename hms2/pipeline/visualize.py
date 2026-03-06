698bf6523b60d7dde81e0e3585b8ec2cbeb0193c
lilian.yeh <lilian.yeh@aetherai.com>
2026-03-06 05:41:20 +0000

    update pipeline code

--- hms2/pipeline/visualize.py
+++ hms2/pipeline/visualize.py
@@ -1,4 +1,14 @@
-from .main import visualize_main
+import argparse
 
-if __name__ == "__main__":
-    visualize_main()
+from .components.main import visualize_main
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--config',
+        type=str,
+        required=True,
+    )
+    args = parser.parse_args()
+    visualize_main(args.config)
